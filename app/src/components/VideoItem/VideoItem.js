605c06a8512e6085b0162a7e8a8674b2ddf37cc5
James Grubb <jamesgrubb@mac.com>
2019-05-22 21:51:42 +0100

    about to use component

--- app/src/components/VideoItem/VideoItem.js
+++ app/src/components/VideoItem/VideoItem.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Embed = styled.iframe.attrs({
-    className: "mw-100"
+  className: "mw-100"
 })`
-border: none;
-margin: none;
-display: inline-block;
-`
+  border: none;
+  margin: none;
+  display: inline-block;
+`;
 
-const EmbedWrapper = (props) => {
-    return(
-    <Embed title="vimeo-player" src={`https://player.vimeo.com/video/${props.id}?autoplay=1&loop=1&autopause=0`} width="640" height="617"  frameborder="0" allowfullscreen ></Embed>
-    )
-}
+const EmbedWrapper = props => {
+  return (
+    <Embed
+      title="vimeo-player"
+      src={`https://player.vimeo.com/video/${
+        props.id
+      }?autoplay=1&loop=1&autopause=0`}
+      width="640"
+      height="617"
+      frameborder="0"
+      allowfullscreen
+    />
+  );
+};
 
-export default EmbedWrapper
+export default EmbedWrapper;
--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "tachyons";
+import * as serviceWorker from "./serviceWorker";
 
-import './index.css';
-import App from './App';
-import 'tachyons'
-import * as serviceWorker from './serviceWorker';
+const Global = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+h1,h2,h3,h4{
+    font-family: 'IBM Plex Sans'
+}
+`;
+
+ReactDOM.render(
+  <React.Fragment>
+    <Global />
+    <App />
+  </React.Fragment>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
